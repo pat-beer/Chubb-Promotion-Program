5fec4850bc2061dd7fd546ff67221863bb12f9fc
pat-beer <patsapol@gmail.com>
2026-08-26 14:21:41 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,5 @@
-/* Chubb Life Calculator – Service Worker v5.0 | 13 Jul 2026 */
-const CACHE = 'chubb-calc-v5.0';
-const ASSETS = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
-self.addEventListener('fetch', e => {
-    if (e.request.method !== 'GET') return;
-    e.respondWith(fetch(e.request).then(r => { const c=r.clone(); caches.open(CACHE).then(cache=>cache.put(e.request,c)); return r; }).catch(()=>caches.match(e.request)));
-});
+const CACHE='chubb-calc-v6.0';
+const ASSETS=['./', './index.html', './manifest.json'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));});
